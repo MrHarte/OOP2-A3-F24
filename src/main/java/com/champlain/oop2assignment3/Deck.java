6ed91af80d1d8e1026617ca8943542b5e989d14b
Willow <williamepduff@gmail.com>
2025-10-07 15:47:58 -0400

    Working on singleton for the Deck class.
    Still very much broken, will fix asap.

--- src/main/java/com/champlain/oop2assignment3/Deck.java
+++ src/main/java/com/champlain/oop2assignment3/Deck.java
@@ -16,18 +16,27 @@ public class Deck extends CardCollection implements CardSource {
     /**
      * The list of cards in the deck.
      */
-    private final List<Card> aCards = new ArrayList<>();
+    private final static List<Card> aCards = new ArrayList<>();
 
     /**
      * Constructs a new Deck containing all standard playing cards.
      * The deck is initialized with one of each rank and suit combination.
      */
-    public Deck() {
+    private Deck() {
         for (Rank currentRank : Rank.values()) {
             for (Suit currentSuit : Suit.values()) {
                 this.aCards.add(new Card(currentRank, currentSuit));
             }
         }
+    }
+
+    public static Deck getDeck()
+    {
+        if (aCards == null)
+        {
+            List<Card> aCards = new ArrayList<>();
+        }
+        return List<Card> aCards;
     }
 
     /**
