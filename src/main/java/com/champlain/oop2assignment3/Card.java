0d13c3490861fc484f76431734e6b852c2ab9962
Pocky <itselishalukibi@gmail.com>
2025-10-09 09:57:49 -0400

    New override method

--- src/main/java/com/champlain/oop2assignment3/Card.java
+++ src/main/java/com/champlain/oop2assignment3/Card.java
@@ -45,6 +45,21 @@ public class Card {
     }
 
     /**
+     * Checks if this card is equal to another object.
+     * Two cards are equal if they have the same rank and suit.
+     *
+     * @param obj the object to compare with
+     * @return true if the cards have the same rank and suit, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Card other = (Card) obj;
+        return aRank == other.aRank && aSuit == other.aSuit;
+    }
+
+    /**
      * Returns a string representation of this card.
      *
      * @return a string in the format "Rank of Suit" (e.g., "Ace of Hearts")
