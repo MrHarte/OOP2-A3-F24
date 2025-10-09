73fae83680f2e9a146c070087a7cae7e29a8f2d3
Chemical Bonds <delldinde8@gmail.com>
2025-10-09 19:39:22 -0400

    Added ScoringStrategy and SimpleCountStrategy.

--- src/main/java/com/champlain/oop2assignment3/DeckController.java
+++ src/main/java/com/champlain/oop2assignment3/DeckController.java
@@ -14,7 +14,7 @@ import javafx.scene.control.TextArea;
  * the current state of the deck and hand.
  * </p>
  */
-public class DeckController {
+public class DeckController{
     /**
      * TextArea for displaying the current state of the deck.
      */
@@ -111,14 +111,15 @@ public class DeckController {
     @FXML
     protected void onScoreButtonClick() {
         String choice = this.aScoreStrategyChoiceBox.getValue();
+
         if (choice == null) {
             Alert selectionErrorAlert = new Alert(Alert.AlertType.ERROR, "Please choose a scoring strategy first.");
             selectionErrorAlert.showAndWait();
         } else {
             switch (choice) {
                 case "Simple Count":
-                    // TODO: Replace the following line of code.
-                    this.aScoreLabel.setText("Simple count...");
+                    SimpleCountStrategy score = new SimpleCountStrategy();
+                    this.aScoreLabel.setText(String.valueOf(score.calculateScore(this.aHand)));
                     break;
                 case "Number Of Aces":
                     // TODO: Replace the following line of code.
